a97fc25bbc778a9dd9959c8c46cb5528e0c71558
eureka <andre@eurekalabs.com.br>
2023-08-27 13:05:22 -0300

    project's initial commit

---
+++ src/main/java/test/foodtosave/FoodToSaveApplication.java
@@ -0,0 +1,13 @@
+package test.foodtosave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodToSaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodToSaveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/foodtosave/FoodToSaveApplicationTests.java
@@ -0,0 +1,24 @@
+package test.foodtosave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest()
+class FoodToSaveApplicationTests {
+
+}
